6e8821b63800bce55cdc0915754ecdc8cc4c0ddd
gustavovalle23 <gustavo.vallerp@hotmail.com>
2022-10-29 15:30:43 -0300

    :sparkles: encode and decode messages from schema registry - AVRO sintax

--- movie-app/encode-message.js
+++ movie-app/encode-message.js
@@ -1,51 +1,39 @@
-const { Kafka } = require('kafkajs')
-const { SchemaRegistry, SchemaType, COMPATIBILITY } = require('@kafkajs/confluent-schema-registry')
-const { readFileSync } = require('fs')
-const { join } = require('path');
-const { debug } = require('console');
+const { SchemaRegistry, SchemaType } = require('@kafkajs/confluent-schema-registry')
 
-async function register(schema, registry) {
-	const { id } = await registry.register(
-		{
-			type: SchemaType.PROTOBUF,
-			schema,
-		},
-		{
-			compatibility: COMPATIBILITY.BACKWARD,
-			subject: 'book.app.Book',
-		},
-	);
-	return { id }
+
+const schema = {
+	name: 'book',
+	namespace: 'test',
+	type: 'record',
+	fields: [
+		{ name: 'name', type: 'string' },
+		{ name: 'days', type: 'string' }
+	]
 }
 
-class Book {
-	constructor(name, days) {
-		this.name = name
-		this.days = days
-	}
+const registry = () => {
+	return new SchemaRegistry({ host: 'http://schema-registry:8081/' })
 }
 
+async function encodePayload(payload) {
+	const schemaRegistry = registry()
 
-async function run() {
-	const book = new Book('Book 1', 20)
+	const { id } = await schemaRegistry.register({
+		schema: JSON.stringify(schema),
+		type: SchemaType.AVRO
+	});
 
-
-	const kafka = new Kafka({ clientId: 'movie-app', brokers: ['kafka:9092'] })
-	const schemaRegistry = new SchemaRegistry({ host: 'http://schema-registry:8081/' }, {
-		[SchemaType.PROTOBUF]: {
-			messageName: 'Book',
-		},
-	})
-	const schema = readFileSync(join(__dirname, './book.proto')).toString();
-	const { id: schemaRegistryId } = await register(schema, schemaRegistry)
-
-	const encodedMessage = await schemaRegistry.encode(schemaRegistryId, {
-		book,
-	  });
-
-	debug(encodedMessage)
-	const decoded = await schemaRegistry.decode(encodedMessage)
-	debug(decoded)
+	const encodedValue = await schemaRegistry.encode(id, payload)
+	return encodedValue
 }
 
-run()
+async function decodePayload(bufferedPayload) {
+	const schemaRegistry = registry()
+	const decodedValue = await schemaRegistry.decode(bufferedPayload)
+	return decodedValue
+}
+
+module.exports = {
+	encodePayload,
+	decodePayload
+}
--- movie-app/index.js
+++ movie-app/index.js
@@ -1,10 +1,17 @@
 const express = require('express')
+
+const { decodePayload, encodePayload } = require('./encode-message')
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-	res.send('Hello World')
+app.get('/', async (req, res) => {
+	const payload = { name: 'Book1', days: '20' }
+	const encodedMessage = await encodePayload(payload)
+	const decodedMessage = await decodePayload(encodedMessage)
+	res.send({ originalMessage: payload, encodedMessage, decodedMessage})
 })
+
 
 app.listen(port, () => {
 	console.log(`Listeing on port ${port}`)
