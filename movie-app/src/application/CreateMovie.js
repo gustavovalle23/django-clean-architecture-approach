f3583a0cc932e675beeb745259583ea3ba4e4977
gustavovalle23 <gustavo.vallerp@hotmail.com>
2023-01-15 11:26:15 -0300

    removing express and nestjs

--- movie-app/src/application/CreateMovie.js
+++
@@ -1,7 +0,0 @@
-const CreateMovieUseCase = ({ MovieRepository, Movie }) =>
-	(title, synopsis, duration) => {
-		const movie = new Movie({ title, synopsis, duration })
-		return MovieRepository.save(movie)
-	}
-
-export default CreateMovieUseCase;
